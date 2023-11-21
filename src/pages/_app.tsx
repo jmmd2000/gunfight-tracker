ab58e615410b29059e8886179a4faaa94b4583f2
James Doyle <jamesmddoyle@gmail.com>
2023-11-21 21:10:01 +0000

    Basic Clerk auth set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
+const inter = Inter({ subsets: ["latin"] });
+
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={inter.className}>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </div>
+  );
 };
 
 export default api.withTRPC(MyApp);
